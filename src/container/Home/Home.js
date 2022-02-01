3d2136e088d7e8bdb3c89758adc05a96f745c8d6
Fedou Brice <napofedoupro@gmail.com>
2022-02-01 15:51:52 +0100

    Mise en place de Redux pour les articles

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,14 +1,35 @@
 import "./Home.scss";
 import Card from "../../components/Card/Card";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { getArticles } from "../../redux/articles/articleReducer";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
+  const { articles } = useSelector((state) => ({
+    ...state.articleReducer,
+  }));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (articles.length === 0) {
+      dispatch(getArticles());
+    }
+  }, []);
+
   return (
     <div>
       <h1 className="home-title">Tous les articles</h1>
       <div className="container-cards">
-        <Card>
-          <h2>Hello card</h2>
-        </Card>
+        {articles.map((item) => {
+          return (
+            <Card key={uuidv4()}>
+              <h2>{item.title}</h2>
+              <a href="#!">En voir plus</a>
+            </Card>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/redux/articles/articleReducer.js
@@ -0,0 +1,27 @@
+const INITIAL_STATE = {
+  articles: [],
+};
+
+function articleReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case "LOADARTICLES": {
+      return {
+        ...state,
+        articles: action.payload,
+      };
+    }
+  }
+  return state;
+}
+export default articleReducer;
+
+export const getArticles = () => (dispatch) => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      dispatch({
+        type: "LOADARTICLES",
+        payload: data,
+      });
+    });
+};
